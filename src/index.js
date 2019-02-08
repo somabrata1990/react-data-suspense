3c0a8f034468a67a8f015995be5d4a99d9d249bb
unknown <somabrata.somu@gmail.com>
2019-02-09 02:50:27 +0530

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-export default './suspense';
+import React from 'react';
+import {loaderContext} from './context';
+
+export default class Suspense extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.promise = null;
+    this.fetchData = (func) => {
+        func()
+        .then(() => this.setState({
+                isLoading: true
+        }))
+        .finally(() => this.setState({
+            isLoading: false
+        }));
+    };
+
+    this.state = {
+      isLoading: false,
+      fetchData: this.fetchData,
+    };
+  }
+
+  render() {
+    // The entire state is passed to the provider
+    return (
+      <loaderContext.Provider value={this.state}>
+        {this.props.children}
+      </loaderContext.Provider>
+    );
+  }
+}
+
+export const Lazy = (Element) => {
+  // It should have a single children
+  // incase you have multiple then wrap it in React.Fragment
+    return function(props){
+      return(
+        <loaderContext.Consumer>
+          {({isLoading, fetchData}) => {
+              const defaultStyles = {
+                  position: 'fixed',
+                  height: '100%',
+                  width: '100%',
+                  backgroundColor: 'grey',
+              };
+            return <Element {...props} fetchData={fetchData} isLoading={isLoading}/>
+          }}
+        </loaderContext.Consumer>
+      )};
+};
